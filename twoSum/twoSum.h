5ca14f81b04d951e76912858f2c90f4b3b266b1b
Judah2015 <1650838291@qq.com>
2017-01-06 00:34:05 +0800

    Create twoSum.h

---
+++ twoSum/twoSum.h
@@ -0,0 +1,35 @@
+/*
+ * twoSum.h
+ *
+ *  Created on: 2017年1月6日
+ *      Author: lzd
+ */
+
+#ifndef TWOSUM_TWOSUM_H_
+#define TWOSUM_TWOSUM_H_
+
+#include<vector>
+#include<tr1/unordered_map>
+
+using namespace std;
+using namespace std::tr1;
+
+class twoSum {
+public:
+    vector<int> f1(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            int numToFind = target - nums[i];
+            if (hash.find(numToFind) != hash.end()) {
+                result.push_back(hash[numToFind]);
+                result.push_back(i);
+                return result;
+            }
+            hash[nums[i]] = i;
+        }
+        return result;
+    }
+};
+
+#endif /* TWOSUM_TWOSUM_H_ */
